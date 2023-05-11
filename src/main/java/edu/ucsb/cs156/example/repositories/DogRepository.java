5b8bb8815494e60cdc715ef8cc59de3e90bbde5d
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-05-11 09:31:56 -0700

    Fix dog repository

--- src/main/java/edu/ucsb/cs156/example/repositories/DogRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/DogRepository.java
@@ -1,10 +1,10 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.Dog;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Dog extends CrudRepository<UCSBDate, Long> {
+public interface DogRepository extends CrudRepository<Dog, Long> {
 }
