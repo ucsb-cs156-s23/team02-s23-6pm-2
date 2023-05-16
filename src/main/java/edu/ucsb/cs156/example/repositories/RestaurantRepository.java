10493c75530ce0da8ded66ee69446afc00f07095
Jacob Toomey <jtoomey@ucsb.edu>
2023-05-16 14:19:33 -0700

    Fix entity

--- src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
@@ -1,10 +1,9 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
-
+import edu.ucsb.cs156.example.entities.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Restaurant extends CrudRepository<UCSBDate, Long> {
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
 }
